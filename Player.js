1f6914e797792bf32b9ee952eadf2348494ccb89
Konrad Nowak <shaderpl@gmail.com>
2022-04-21 16:37:22 +0200

    Add Player class, add basic scene rendering

---
+++ Player.js
@@ -0,0 +1,34 @@
+class Player {
+    constructor() {
+        this.player = PIXI.Sprite.from('assets/sample.png');
+        this.player.anchor.set(0.5);
+        this.player.x = app.screen.width / 2;
+        this.player.y = app.screen.height - 40;
+        this.player.width = 100;
+        this.player.height = 100;
+        this.player.interactive = true;
+        this.speed = 0;
+        this.maxSpeed = 4;
+    }
+
+    moveLeft() {
+        this.speed = -this.maxSpeed;
+    }
+
+    moveRight() {
+        this.speed = this.maxSpeed;
+    }
+
+    stop() {
+        this.speed = 0;
+    }
+
+    update(delta) {
+        this.player.x += this.speed * delta; 
+    }
+
+    getSprite() {
+        return this.player;
+    }
+    
+}
--- app.js
+++ app.js
@@ -9,3 +9,49 @@ var renderer = PIXI.autoDetectRenderer(512, 512, {
 document.getElementById('display').appendChild(renderer.view);
 
 
+ let app = new PIXI.Application({
+    width: 800, height: 600, backgroundColor: 0x5c5c5c
+});
+
+document.body.appendChild(app.view);
+
+let keys = {}
+
+
+const player = new Player();
+
+app.stage.addChild(player.getSprite());
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowRight') {
+        player.moveRight();
+    }
+    if (e.key === 'ArrowLeft') {
+        player.moveLeft();
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.key === 'ArrowRight') {
+        player.stop();
+    }
+    if (e.key === 'ArrowLeft') {
+        player.st();
+    }
+});
+
+app.ticker.add((delta) => {
+    player.update(delta);
+});
+
+
+
+const enemy = PIXI.Sprite.from('assets/a.png');
+enemy.anchor.set(0.5);
+enemy.x = 50;
+enemy.y = 50;
+enemy.width = 100;
+enemy.height = 50;
+
+app.stage.addChild(enemy);
